fcf3489865d9e5a134a129cff9ea31ea81839d07
shannoncassandra <61391254+shannoncassandra@users.noreply.github.com>
2024-03-03 15:06:54 -0800

    Add files via upload
    Muliply each letter 3x.

---
+++ Paper Doll.py
@@ -0,0 +1,11 @@
+#Muliply each letter 3x.
+
+text=input("Word please! ")
+
+def paper_doll(text):
+    result = ''
+    for char in text:
+        result += char*3
+    return result
+
+print(paper_doll(text))
